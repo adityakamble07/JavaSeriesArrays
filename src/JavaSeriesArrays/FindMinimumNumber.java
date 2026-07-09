449136f14b5713935988411cf77abd3fb528c82c
adityakamble07 <adityakamble95482@gmail.com>
2026-07-09 13:37:43 +0530

    Add minimum number array program

---
+++ src/JavaSeriesArrays/FindMinimumNumber.java
@@ -0,0 +1,33 @@
+package JavaSeriesArrays;
+import java.util.*;
+public class FindMinimumNumber {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int size = sc.nextInt();
+
+        int arr[] = new int[size];
+
+        System.out.println("Enter array elements:");
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int min = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+
+        }
+
+        System.out.println("Minimum Number = " + min);
+
+        sc.close();
+    }
+}
